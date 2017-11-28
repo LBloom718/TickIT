057c0e8200c8a47280e46b614cd0f9d3038e4e88
minmeikarinafindlay <minmeikarinafindlay@gmail.com>
2017-11-27 23:45:26 -0500

    Added a Ticket Helper ! :)

---
+++ TickIT/Helpers/TicketHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TickIT.Models;
+
+namespace TickIT.Helpers
+{
+    public static class TicketHelpers
+    {
+
+        public static int CountOpenTickets(User user)
+        {
+            var open = 0;
+
+            var openTickets = user.Tickets;
+            foreach (var item in openTickets)
+            {
+                if (item.status.Equals(Status.OPEN))
+                {
+                    open += 1;
+                }
+            }
+            return open;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

c36c68d4c10ae766496388c193ed7021df463798
minmeikarinafindlay <minmeikarinafindlay@gmail.com>
2017-11-27 22:49:28 -0500

    Added a person class deleted the user type

---
+++ TickIT/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TickIT.Models
+{
+    public class Person
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+    }
+}
--- TickIT/Models/User.cs
+++ TickIT/Models/User.cs
@@ -7,23 +7,11 @@ using System.Runtime.Serialization;
 namespace TickIT.Models
 {
     [DataContract]
-    public class User
+    public class User : Person
     {
         [DataMember]
         public int UserID { get; set; }
-        [DataMember]
-        public string FirstName { get; set; }
-        [DataMember]
-        public string LastName { get; set; }
-        [DataMember]
-        public string Email { get; set; }
-        [DataMember]
-        public UserType Type { get; set; }
 
+        public virtual ICollection<Ticket> Tickets { get; set; }
     }
-
-    public enum UserType
-    {
-        CLIENT = 1, ADMINISTRATOR = 2
-    };
 }

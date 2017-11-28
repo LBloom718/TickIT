1c634b72bd69f6bdb1108a65e15d8426c54366bb
minmeikarinafindlay <minmeikarinafindlay@gmail.com>
2017-11-27 23:01:31 -0500

    Added DB Context and a Data Layer :)

---
+++ TickIT/DAL/TicketContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TickIT.Models;
+
+namespace TickIT.DAL
+{
+    public class TicketContext : DbContext
+    {
+
+        public TicketContext() : base("TicketContext")
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Ticket> Ticket { get; set; }
+    }
+}
